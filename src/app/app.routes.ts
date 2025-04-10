f72ec33937fa3f3f5b8b5b12442eb008e0470d74
VolodumurGruv <gru.v@i.ua>
2025-04-10 21:52:59 +0300

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { LayoutComponent } from './shared/components/outlet/layout/layout.component';
-
-export const routes: Routes = [
-  { path: '', component: LayoutComponent },
-  { path: '', redirectTo: '/', pathMatch: 'full' },
-  { path: '**', redirectTo: '/', pathMatch: 'full' },
-];
